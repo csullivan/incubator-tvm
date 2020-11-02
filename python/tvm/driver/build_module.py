2c59981d2335723c222e2ac4bbbbc6f03cf32944
Chris Sullivan <csullivan@octoml.ai>
2020-11-02 14:39:40 -0800

    Add scaffolding for texture_flatten pass.

--- python/tvm/driver/build_module.py
+++ python/tvm/driver/build_module.py
@@ -196,6 +196,7 @@ def lower(
     if is_legacy_te_schedule:
         pass_list += [
             tvm.tir.transform.InjectPrefetch(),
+            tvm.tir.transform.TextureFlatten(64, instrument_bound_checkers),
             tvm.tir.transform.StorageFlatten(64, instrument_bound_checkers),
         ]
     else:

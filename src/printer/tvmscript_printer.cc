78a8c6a72ae2d5d0cfe08748742cd42db30bd49b
Chris Sullivan <csullivan@octoml.ai>
2020-11-12 11:42:04 -0800

    Add TextureType support.

--- src/printer/tvmscript_printer.cc
+++ src/printer/tvmscript_printer.cc
@@ -142,6 +142,7 @@ class TVMScriptPrinter : public StmtFunctor<Doc(const Stmt&)>,
 
   Doc VisitType_(const PrimTypeNode* node) override;
   Doc VisitType_(const PointerTypeNode* node) override;
+  Doc VisitType_(const TextureTypeNode* node) override;
   Doc VisitType_(const TupleTypeNode* node) override;
 
   Doc PrintBody(const Stmt& body);
@@ -765,6 +766,12 @@ Doc TVMScriptPrinter::VisitType_(const PrimTypeNode* node) {
 Doc TVMScriptPrinter::VisitType_(const PointerTypeNode* node) {
   Doc doc;
   doc << "ty.Ptr[" << Print(node->element_type) << "]";
+  return doc;
+}
+
+Doc TVMScriptPrinter::VisitType_(const TextureTypeNode* node) {
+  Doc doc;
+  doc << "ty.Texture[" << Print(node->element_type) << "]";
   return doc;
 }
 
